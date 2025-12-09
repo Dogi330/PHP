628f87b1670b51fe790efe0c85993f0a1524f12a
Evgeniy Titov <tronliz57@gmail.com>
2025-12-09 10:13:42 +0300

    Update cookie.php

--- lab6/cookie.php
+++ lab6/cookie.php
@@ -1,52 +1,37 @@
 <?php
 declare(strict_types=1);
 
-/*
-ЗАДАНИЕ 1
-- Инициализируйте переменную для подсчета количества посещений
-- Если соответствующие данные передавались через куки
-  сохраняйте их в эту переменную
-- Нарастите счетчик посещений
-- Инициализируйте переменную для хранения значения последнего посещения страницы
-- Если соответствующие данные передавались из куки, отфильтруйте их и сохраните в эту переменную.
-  Для фильтрации используйте функции trim(), htmlspecialchars()
-- С помощью функции setcookie() установите соответствующие куки.  Задайте время хранения куки 1 сутки.
-  Для задания времени последнего посещения страницы используйте функцию date()
-*/
+use MyProject\Classes\User;
+use MyProject\Classes\SuperUser;
 
-$visits = 1;
-if (isset($_COOKIE['visits'])) {
-    $visits = (int) $_COOKIE['visits'] + 1;
-}
+spl_autoload_register(function ($class) {
 
-$visitMessage = 'Добро пожаловать!';
-if (isset($_COOKIE['visitMessage'])) {
-    $visitMessage = htmlspecialchars(trim($_COOKIE['visitMessage']));
-    $visitMessage = "<p>Последнее посещение: $visitMessage</p>";
-}
+    //namespace
+    $prefix = 'MyProject\\Classes\\';
 
-setcookie('visits', (string) $visits, time() + 86400);
-setcookie('visitMessage', date("Y-m-d H:i:s"), time() + 86400); // Время хранения куки — 1 сутки (86400 секунд)
-?>
-<!DOCTYPE html>
-<html lang="ru">
+    if (strpos($class, $prefix) !== 0) {
+        return;
+    }
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Последний визит</title>
-</head>
+    $relativePath = str_replace($prefix, '', $class);
 
-<body>
+    // Преобразуем namespace
+    $relativePath = str_replace('\\', DIRECTORY_SEPARATOR, $relativePath);
 
-    <h1>Последний визит</h1>
+    // Путь к файлам классов
+    $file = __DIR__ . '/MyProject/Classes/' . $relativePath . '.php';
 
-    <?php
-    echo "<p>Количество посещений: $visits</p>";
-    echo $visitMessage;
-    ?>
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
 
-</body>
+// Создание объектов
+$user1 = new User("Александр Воронов", "voronov", "password1");
+$user2 = new User("Дмитрий Соколов", "sokolov", "password2");
+$user3 = new SuperUser("Админ", "admin", "superpass", "administrator");
 
-</html>
+// Вывод информации
+$user1->showInfo();
+$user2->showInfo();
+$user3->showInfo();
