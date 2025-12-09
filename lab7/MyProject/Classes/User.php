57e76f68c6736a4c98b1f2e5bd6b287df8514f45
Evgeniy Titov <tronliz57@gmail.com>
2025-12-09 10:55:14 +0300

    Update User.php

--- lab7/MyProject/Classes/User.php
+++ lab7/MyProject/Classes/User.php
@@ -1,24 +1,73 @@
 <?php
-declare(strict_types=1);
-
 namespace MyProject\Classes;
+
 class User
 {
-    public string $name;
-    public string $login;
-    private string $password;
-    public function __construct(string $name, string $login, string $password)
+    /**
+     * Имя пользователя
+     * @var string $name Полное имя пользователя
+     * @access public
+     */
+    public $name;
+    
+    /**
+     * Логин пользователя
+     * @var string $login Логин пользователя
+     * @access public
+     */
+    public $login;
+    
+    /**
+     * Пароль пользователя
+     * @var string $password Пароль пользователя
+     * @access private
+     */
+    private $password;
+    
+    /**
+     * Конструктор класса User
+     * @param string $name Полное имя пользователя
+     * @param string $login Логин пользователя 
+     * @param string $password Пароль пользователя
+     * 
+     * @return void
+     * @access public
+     */
+    public function __construct($name, $login, $password)
     {
         $this->name = $name;
         $this->login = $login;
         $this->password = $password;
     }
-    public function showInfo(): void
-    {
-        echo "<p>Имя: {$this->name}<br>Логин: {$this->login}</p>";
-    }
+    
+    /**
+     * Деструктор класса User
+     * @return void
+     * @access public
+     */
     public function __destruct()
     {
-        echo "<p>Пользователь {$this->login} удален.</p>";
+        echo "Пользователь {$this->login} удален.<br>";
+    }
+    
+    /**
+     * Метод для отображения информации о пользователе
+     * @return void
+     * @access public
+     * 
+     * @example
+     * $user = new User("Иван Иванов", "ivanov", "secret123");
+     * $user->showInfo();
+    
+     */
+    public function showInfo()
+    {
+        echo "--- Информация о пользователе ---<br>";
+        echo "Имя: {$this->name}<br>";
+        echo "Логин: {$this->login}<br>";
+        echo "Пароль: [скрыто]<br>";
+        echo "--------------------------------<br><br>";
     }
 }
+?>
+
